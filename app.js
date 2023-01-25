a6e0e93b3b92a9f0d0d4cb00c893887ed811d5bb
Haniya Fatima <haniyaf2021@gmail.com>
2023-01-24 21:51:45 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// document.write ("h1");
+// alert("Hello World");
+
+// alert(1+2);
+
+// alert("This is our output" + 19);
+
+// console.log("Hello WoRLD!");
+
+// console.log("my age is " + (7 + 5));
+
+// var name = "My name is Haniya ";
+// var age = "I am 22 years old.";
+// var intro = name + age;
+// console.log(intro);
+
+// console.warn("Warning starnger danger!");
+// console.error("ERROR ERROR ERROR ERROR");
+
+document.getElementById("p1").innerHTML = "My name is Haniya"
